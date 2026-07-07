e2e7f1de67f4187fac9f69095f8e81858369d539
DevDaddyJacob <jacobwpaulin@gmail.com>
2026-07-07 13:07:31 -0400

    chore: cleaned AST files

--- src/engine/parser/ast.c
+++ src/engine/parser/ast.c
@@ -34,37 +34,3 @@
  * Function Definitions
  * ==================================================
  */
-
-void arr_ast_node_init(arr_ast_node_t* array)
-{
-	if (NULL == array)
-	{
-		return;
-	}
-
-	array->count = 0;
-	array->capacity = 0;
-	array->nodes = NULL;
-}
-
-
-void arr_ast_node_destroy(arr_ast_node_t* array)
-{
-	if (NULL == array)
-	{
-		return;
-	}
-
-	for (size_t i = 0; i < array->count; i++)
-	{
-		token_destroy(array->values + i);
-	}
-
-	free(array);
-}
-
-
-void arr_ast_node_add(arr_ast_node_t* array, const arr_ast_node_t* value)
-{
-
-}
